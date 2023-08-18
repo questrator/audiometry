65b3d405efd48f68e55b8bd20ffa08b66a370acd
Slava Ivanov <drumtrack@ya.ru>
2023-08-18 17:10:18 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,144 +1,144 @@
-class Sample {
-    constructor() {
-
-    }
-
-    play() { }
-}
-
-class Track {
-    constructor({ group, pause }) {
-        this._pause = pause;
-        this._group = group;
-    }
-
-
-    get pause() {
-        return this._pause;
-    }
-
-    set pause(duration) {
-        this._pause = duration;
-    }
-}
-
-const samples = {
-    0: "./sounds/soul-sirkus-periled-divide.mp3",
-    мороз: "./sounds/мороз.mp3",
-    ковёр: "./sounds/ковёр.mp3",
-    муха: "./sounds/муха.mp3",
-    печка: "./sounds/печка.mp3",
-    зима: "./sounds/зима.mp3",
-    халат: "./sounds/халат.mp3",
-    башня: "./sounds/башня.mp3",
+const words = {
+  мороз: "./sounds/мороз.mp3",
+  ковёр: "./sounds/ковёр.mp3",
+  муха: "./sounds/муха.mp3",
+  печка: "./sounds/печка.mp3",
+  зима: "./sounds/зима.mp3",
+  халат: "./sounds/халат.mp3",
+  башня: "./sounds/башня.mp3",
+  лампа: "./sounds/лампа.mp3",
+  река: "./sounds/река.mp3",
+  сахар: "./sounds/сахар.mp3",
 };
 
 const groups = {
-    G1: {
-        id: "G1",
-        title: "Группа G1",
-        type: "Ошерович, 7-14 лет",
-        words: ["мороз", "ковёр", "муха", "печка", "зима", "халат", "башня"],
-    },
-    G2: {
-        id: "G2",
-        title: "Группа G2",
-        type: "Ошерович, 7-14 лет",
-        words: ["север", "малыш", "роза", "запах", "яма", "рука", "пальто", "ветка", "бычок", "ласка"],
-    },
-    G3: {
-        id: "G3",
-        title: "Группа G3",
-        type: "Ошерович, 7-14 лет",
-        words: ["мусор", "комар", "пчела", "баба", "ванна"],
-    },
-}
-
-const settingsGroups = {
-    valueField: "id",
-    searchField: ["words", "title", "type"],
-    options: [groups.G1, groups.G2, groups.G3],
-    render: {
-        option: function (data, escape) {
-            return "<div>" +
-                "<span class='select-title'>" + escape(data.title) + "</span>" +
-                "<span class='select-type'>" + escape(data.type) + "</span>" +
-                "<span class='select-words'>" + escape(data.words.join(", ")) + "</span>" +
-                "</div>";
-        },
-        item: function (data, escape) {
-            return "<div title='" + escape(data.words) + "'>" + escape(data.title) + "</div>";
-        }
-    }
+  G1: {
+    id: "G1",
+    title: "Группа G1",
+    type: "Ошерович, 7-14 лет",
+    words: ["мороз", "ковёр", "муха", "печка", "зима", "халат", "башня", "лампа", "река", "сахар"],
+  },
+  G2: {
+    id: "G2",
+    title: "Группа G2",
+    type: "Ошерович, 7-14 лет",
+    words: ["север", "малыш", "роза", "запах", "яма", "рука", "пальто"],
+  },
+  G3: {
+    id: "G3",
+    title: "Группа G3",
+    type: "Ошерович, 7-14 лет",
+    words: ["мусор", "комар", "пчела", "баба", "ванна"],
+  },
 };
-const tomSelectGroup = new TomSelect("#select-track", settingsGroups);
 
-const selectGroup = document.querySelector("#select-track");
-selectGroup.addEventListener("change", setGroup);
-
-const trackBlock = document.querySelector("#track");
-trackBlock.innerHTML = "<div class='track-placeholder'>группа слов ещё не выбрана</div>";
-
-function setGroup(event) {
-    console.log(selectGroup.value);
-    if (selectGroup.value === "") {
-        trackBlock.innerHTML = "<div class='track-placeholder'>группа слов ещё не выбрана</div>";
-        return;
-    }
-    trackBlock.innerHTML = "";
-    const words = groups[selectGroup.value].words;
-    for (let i = 0; i < words.length; i++) {
-        const sampleBlock = document.createElement("div");
-        sampleBlock.classList.add("sample-block");
-        sampleBlock.textContent = words[i];
-        const sampleClose = document.createElement("div");
-        sampleClose.classList.add("sample-close");
-        sampleBlock.appendChild(sampleClose);
-        trackBlock.appendChild(sampleBlock);
-    }
-}
+const selectorSettings = {
+  valueField: "id",
+  searchField: ["words", "title", "type"],
+  options: [groups.G1, groups.G2, groups.G3],
+  maxItems: 10,
+  render: {
+    option: function (data, escape) {
+      return (
+        "<div>" +
+          "<span class='select-title'>" + escape(data.title) + "</span>" +
+          "<span class='select-type'>" + escape(data.type) + "</span>" +
+          "<span class='select-words'>" + escape(data.words.join(", ")) + "</span>" +
+        "</div>"
+      );
+    },
+    item: function (data, escape) {
+      return (
+        "<div title='" + escape(data.words) + "'>" + escape(data.title) + "</div>"
+      );
+    },
+  },
+};
 
 
 
+class Sample {
+  constructor(word, file) {
+    this.word = word;
+    this.file = file;
+    this.audio = new Audio(this.file);
+    this.duration = null;
+    this.getDuration();
+  }
 
-const sampleTest = document.querySelector(".sample-test");
-const sampleTestLength = sampleTest.offsetWidth;
-const buttonPlay = document.querySelector(".button-play");
-buttonPlay.addEventListener("click", playSample);
-
-function playSample(event) {
-    const audio = new Audio(samples["башня"]);
-    let duration = 0;
-    audio.addEventListener("loadedmetadata", () => {
-        duration = audio.duration;
-        console.log(duration);
+  getDuration() {
+    this.audio.addEventListener("loadedmetadata", () => {
+      this.duration = this.audio.duration;
     });
-    // audio.addEventListener("timeupdate", () => {
-    //     sampleTest.style.backgroundImage = `linear-gradient(90deg, rgba(50,167,228,1) ${audio.currentTime / duration * 100}%, rgba(27,146,208,1) ${audio.currentTime / duration * 100}%)`;
-    // });
+  }
 
-    requestAnimationFrame(() => { }, sampleTest);
+  play() {
+    this.audio.play();
+  }
 
-    audio.play();
+  stop() {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+  }
+}
 
+class Track {
+  constructor(samples = [], pause = 500) {
+    this.pause = pause;
+    this.samples = samples;
+    this.createSampleList();
+    this.selector = new TomSelect("#select-track", selectorSettings);
+    this.selector.on("change", this.getGroupList);
+  }
 
+  selectorOnChange() {
+    
+  }
 
+  addSample(sample) {
+    this.samples.push(sample);
+  }
 
+  getGroupList() {
+    console.log(this.selector.getValue())
+    return this.selector.getValue();
+  }
 
-    var start = null;
-    var element = sampleTest;
-
-    function step(timestamp) {
-        if (!start) start = timestamp;
-        var progress = timestamp - start;
-        element.style.backgroundImage =
-        `linear-gradient(90deg, rgba(79, 163, 241, 1) ${audio.currentTime / duration * 100}%, rgba(253, 199, 78, 1) ${audio.currentTime / duration * 100}%)`;
-        if (progress < 2000) {
-            window.requestAnimationFrame(step);
-        }
-    }
-
-    window.requestAnimationFrame(step);
+  createSampleList() {    
+    
+  }
 }
 
 
+// const sampleTest = document.querySelector(".sample-test");
+// const sampleTestLength = sampleTest.offsetWidth;
+// const buttonPlay = document.querySelector(".button-play");
+// buttonPlay.addEventListener("click", playSample);
+
+// function playSample(event) {
+//     const audio = new Audio(words["башня"]);
+//     let duration = 0;
+//     audio.addEventListener("loadedmetadata", () => {
+//         duration = audio.duration;
+//         console.log(duration);
+//     });
+//     audio.play();
+
+//     var start = null;
+//     var element = sampleTest;
+
+//     function step(timestamp) {
+//         if (!start) start = timestamp;
+//         var progress = timestamp - start;
+//         element.style.backgroundImage =
+//         `linear-gradient(90deg, rgba(79, 163, 241, 1) ${audio.currentTime / duration * 100}%, rgba(253, 199, 78, 1) ${audio.currentTime / duration * 100}%)`;
+//         if (progress < 2000) {
+//             window.requestAnimationFrame(step);
+//         }
+//     }
+
+//     window.requestAnimationFrame(step);
+// }
+
+const track = new Track();
+console.log(track.getGroupList());
