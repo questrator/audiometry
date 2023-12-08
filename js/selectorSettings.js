0f4922e2304818240bdee1a742de9b3bd5ea02d2
Slava Ivanov <drumtrack@ya.ru>
2023-12-08 14:53:15 +0300

    upd

--- js/selectorSettings.js
+++ js/selectorSettings.js
@@ -5,7 +5,18 @@ const selectorSettings = {
     searchField: ["words", "title", "type"],
     options: Object.values(groups),
     maxItems: 10,
+    optgroups: [
+		{value: "группа", label: "Группы слов"},
+		{value: "слово", label: "Отдельные слова"},
+	],
+	optgroupField: "group",
+    plugins: ['caret_position','input_autogrow', 'drag_drop'],
+    persist: false,
+	create: false,
     render: {
+        optgroup_header: function(data, escape) {
+			return "<div class='select-optgroup'>" + escape(data.label) + "</div>";
+		},
         option: function (data, escape) {
         return (
             "<div>" + 
